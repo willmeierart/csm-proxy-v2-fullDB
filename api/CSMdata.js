48a46d5f066a35e7d95651f187277c2330f0c89c
WILL MEIER <willmeierart@gmail.com>
2017-08-18 14:22:41 -0600

    fix async await

--- api/CSMdata.js
+++ api/CSMdata.js
@@ -5,13 +5,21 @@ const fetch = require('node-fetch')
 
 const API_URL = process.env.API_URL
 
-async function fetchData(){
-  const res = await fetch(API_URL)
-  const json = await res.json()
-  return json
-}
+// async function fetchData(){
+//   const res = await fetch(API_URL)
+//   const json = await res.json()
+//   return json
+// }
+
+
 
 router.get('/', function(req, res) {
+  function fetchData(){
+    return fetch(API_URL)
+    .then((res)=>{
+      return res.json().then((json)=> json)
+    })
+  }
   fetchData().then(json=>{
     return res.json(json)})
   .catch(err=>console.log(err))
